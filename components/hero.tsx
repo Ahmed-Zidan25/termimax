f85118a7c1ff5994d846c49935f52d0af7a031a3
Ahmed Zidan <ahmadibrahim.zidan@gmail.com>
2026-01-08 19:51:16 +0200

    1st

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,6 +6,17 @@ import Link from "next/link";
 import LogoSwiper from "@/components/LogoSwipper";
 
 export default function Hero() {
+  const logos = [
+    { src: "/logos/pax.PNG", alt: "PAX Logo" },
+    { src: "/logos/ncr.svg", alt: "NCR Corporation Logo" },
+    { src: "/logos/ingenico.svg", alt: "Ingenico Logo" },
+    { src: "/logos/verifone.PNG", alt: "Verifone Logo", customSize: "h-15 w-42" },
+    { src: "/logos/sunmi.PNG", alt: "Sunmi" },
+    { src: "/logos/IMIN.svg", alt: "IMIN" },
+    { src: "/logos/Feitian.png", alt: "Feitian" },
+    { src: "/logos/NewPOS.png", alt: "NewPOS" },
+    { src: "/logos/NEXGO.svg", alt: "NEXGO" },
+  ];
   return (
     <section className="relative min-h-screen flex items-center justify-center pt-20 overflow-hidden">
       {/* Animated Background */}
